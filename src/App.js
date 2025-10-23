2ff98dfdfc757522f60964e3b5a8e6da8ad0bc6a
pavan kalyan <bavisettipavan2000@gmail.com>
2025-10-23 16:49:11 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div className="App">
+      <header className="flex items-center justify-center h-screen flex-col bg-gray-100">
+        <h1 className="text-red-600 bg-blue-400 p-4 rounded-lg shadow-md">
+          Text Page
+        </h1>
+        <p className="mt-4 text-gray-700">
+          Edit <code className="font-mono">src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
